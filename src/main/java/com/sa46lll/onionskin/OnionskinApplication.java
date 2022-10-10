b357657c7dd822a13338ff922713258669af600b
sa46lll <9812smh@naver.com>
2022-10-10 12:19:53 +0000

    feat: 프로젝트 생성

---
+++ src/main/java/com/sa46lll/onionskin/OnionskinApplication.java
@@ -0,0 +1,13 @@
+package com.sa46lll.onionskin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnionskinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnionskinApplication.class, args);
+	}
+
+}
